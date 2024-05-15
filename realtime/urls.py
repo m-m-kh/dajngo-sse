9b17e6faefa827f0d7a73ae203e5668b0f4ecc70
m-m-kh-py <khademim217@gmail.com>
2024-05-15 23:32:19 +0330

    impovore server sent event wait

--- realtime/urls.py
+++ realtime/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('event/', views.sse, name='test'),
-    path('t1/', views.t1, )
+    path('event/', views.Event.as_view(), name='test'),
+    path('t1/', views.t1)
 ]
--- realtime/views.py
+++ realtime/views.py
@@ -12,6 +12,8 @@ import random
 from asgiref.sync import sync_to_async
 from django.dispatch import receiver
 from django.db.models import signals
+from django.core.serializers import serialize
+from asgiref.sync import sync_to_async, async_to_sync
 
 # class FutureWrapper:
 #     def __init__(self):
@@ -19,37 +21,49 @@ from django.db.models import signals
 # 
 #     def generate(self):
 #         self.future = asyncio.Future()
-async def sse(request, *args, **kwargs):
+
+# async def sse(request, *args, **kwargs):
+
+class Event(generic.View):
+
     future = asyncio.Event()
+    a = [5]
+    async def get(self, request, *args, **kwargs):
+        # future = asyncio.Event()
+        # @receiver(signal=signals.post_save, sender=models.TestModel)
+        # def send_notification(sender, instance, created, **kwargs):
+        #     print(kwargs)
+        #     if created:
+        #         future.set()
 
-    @receiver(signal=signals.post_save, sender=models.TestModel)
-    def send_notification(sender, instance, created, **kwargs):
-        if created:
-            future.set()
+        async def event_stream():
+            while True:
+                await self.future.wait()
+                model = await models.TestModel.objects.alast()
+                yield "data: {}\n\n".format(model.text)
+                if self.future.is_set():
+                    self.future.clear()
 
-    async def event_stream():
-        while True:
-            await future.wait()
-            model = await models.TestModel.objects.alast()
-            yield "data: {}\n\n".format(model.name)
-            if future.is_set():
-                future.clear()
+        r = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+        r['Cache-Control'] = 'no-cache'
+        r['Connection'] = 'keep-alive'
+        return r
 
+    async def post(self, request, *args, **kwargs):
+        print(self.a)
+        self.a[0] +=1
+        form = forms.TestForm(request.POST or None)
+        if request.method == 'POST':
+            if form.is_valid():
+                await sync_to_async(form.save)()
+                self.future.set()
 
-
-
-    r = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
-    r['Cache-Control'] = 'no-cache'
-    r['Connection'] = 'keep-alive'
-    return r
+        context = {'form': form}
+        return shortcuts.render(request, 'realtime/index.html', context=context)
 
 def t1(request):
-    form = forms.TestForm(request.POST or None)
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-
+    print(request)
+    form = forms.TestForm()
 
     context = {'form': form}
     return shortcuts.render(request, 'realtime/index.html', context=context)
